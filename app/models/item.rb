1af683db1d8a6405b87f843c5cace86bd904fe81
ademir10 <ddsuportservice@gmail.com>
2017-08-31 18:07:01 -0300

    inserindo o nome do produto quando adicionado pela api

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :product
   belongs_to :desk_order
+
+  #para salvar o nome do produto quando é feito uma adição de produto na mesa pelo balcão e naõ pelo produto
+  before_create :insert_product_name, :if => lambda { |item| item.name_prod.nil? }
+  def insert_product_name
+    self.name_prod = self.product.name
+  end
 end
