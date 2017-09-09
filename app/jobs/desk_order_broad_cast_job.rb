02c1f7365c0ac4c159c612b550e435279f8ed55d
ademir10 <ddsuportservice@gmail.com>
2017-09-09 01:05:31 -0300

    ajuste

--- app/jobs/desk_order_broad_cast_job.rb
+++ app/jobs/desk_order_broad_cast_job.rb
@@ -3,6 +3,6 @@ class DeskOrderBroadCastJob < ApplicationJob
 
   def perform(message)
     @desk_order = DeskOrder.find_by(id: $desk_order_id.to_i)
-      ActionCable.server.broadcast "desk_order_channel", message: 'A ' + @desk_order.qrpoint.description.to_s + ' acabou de solicitar a sua conta.                        ' 'Forma de pagamento: ' + @desk_order.form_payment.type_payment.to_s
+      ActionCable.server.broadcast "desk_order_channel", message: 'A ' + @desk_order.qrpoint.description.to_s + ' acabou de solicitar a sua conta.                             ' 'Forma de pagamento: ' + @desk_order.form_payment.type_payment.to_s
   end
 end
