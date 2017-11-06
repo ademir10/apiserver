a723ff4f9203110eb243e78da5e0a253ae035000
ademir10 <ddsuportservice@gmail.com>
2017-11-06 17:35:07 -0200

    inserindo a verificacao da data de expiracao

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,8 @@
 class SessionsController < ApplicationController
 
   def expired_date
-    #just to show expired view
+    sweetalert_warning('A sua licença expirou, efetue o pagamento e entre em contato com o suporte para reativa-la.!', 'Atenção!', useRejections: false)
+    redirect_to root_path and return
   end
 
   def new
@@ -9,11 +10,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-
     if params[:email].blank? || params[:password].blank?
       sweetalert_error('Informe os seus dados!', 'Atenção!', useRejections: false)
-
-         redirect_to root_path and return
+      redirect_to root_path and return
     end
 
     user = User.find_by_email(params[:email])
@@ -21,23 +20,44 @@ class SessionsController < ApplicationController
     session[:user_id] = user.id
 
     #inserindo no log de atividades
-        #log = Loginfo.new(params[:loginfo])
-        #log.employee = current_user.name
-        #log.task = 'Efetuou o Login no sistema'
-        #log.save!
+        log = Loginfo.new(params[:loginfo])
+        log.employee = current_user.name
+        log.task = 'Efetuou o Login no sistema'
+        log.save!
+puts 'AQUI É A DATA DE AMANHÃ ' + Date.tomorrow.to_s
+  if current_user.type_access != 'MASTER'
+      #verifica se a licença de uso está ok somente se o usuario não for MASTER
+      check_date = Config.first
 
+  if check_date.check_date == '1' && check_date.expiration_date.to_date == Date.today.to_date
+    #faltando 1 dia para vencer a licença o cliente é alertado
+    sweetalert("Olá" + " " +  user.name + "!", 'Hoje, dia ' + Date.today.to_s + ' vence a sua licença, a partir de amanhã o sistema será bloqueado automaticamente, efetue o pagamento e entre em contato com o departamento financeiro para reativa-lo.', imageUrl: 'images/logo.png', persistent: 'OK')
+    redirect_to root_path and return
 
+  elsif check_date.check_date == '1' && Date.tomorrow.to_date == check_date.expiration_date.to_date
+    #faltando 1 dia para vencer a licença o cliente é alertado
+    sweetalert("Olá" + " " +  user.name + "!", 'Gostariamos de lembrar que amanhã ' + Date.tomorrow.to_s + ' vence a sua licença, não se esqueça de renova-la pois o sistema será bloqueado automaticamente após esta data.', imageUrl: 'images/logo.png', persistent: 'OK')
+    redirect_to root_path and return
+
+  elsif check_date.check_date == '1' && check_date.expiration_date.to_date < Date.today.to_date
+      #para informar ao form que expirou e precisa mostrar os dados bancários para pagamento
+      params[:expire_date] = check_date.expiration_date
+
+       sweetalert_error('A sua licença expirou, efetue o pagamento e informe o departamento financeiro para que seja feito o desbloqueio automatico.', 'Atenção' , persistent: 'OK')
+      session[:user_id] = nil
+     redirect_to root_path and return
+      end
+     end
     #se estiver tudo ok e a licença ok
+    sweetalert("Oi" + " " +  user.name + "!", 'Bem vindo!', imageUrl: 'images/logo.png')
+    redirect_to root_path
 
-  sweetalert_success('Bem vindo ' + current_user.name.to_s + "!", 'Olá!', useRejections: false)
-
-     redirect_to dashboard_path
-   else
-
-     sweetalert_warning('Dados inválidos, tente outra vez!', 'Erro.', useRejections: false)
+    else
+      sweetalert_error('Email ou Senha incorretos, por favor verifique os dados.', 'Atenção')
       redirect_to root_path
+      #render 'new'
+    end
   end
-end
 
   def destroy
      #inserindo no log de atividades
